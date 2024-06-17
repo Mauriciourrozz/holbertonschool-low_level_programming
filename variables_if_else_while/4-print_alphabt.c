e6da0fa724e7a7fd59e3b95abbb988c9d20eced7
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-17 13:16:06 +0000

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,12 +11,12 @@ int main(void)
 
 	while (letra <= 'z')
 	{
-		if (letra != 'e')
+		if (letra == 'e')
 		{
 			putchar(letra);
 			letra++;
 		}
-		if (letra != 'q')
+		if (letra == 'q')
 		{
 			putchar(letra);
 			letra++;
