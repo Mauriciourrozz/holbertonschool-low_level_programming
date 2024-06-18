6e5ef4a43349df0f4a7fa6aed987af5d7b1cb9c6
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-18 18:20:31 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - calcula el valor absoluto
+ *@num: numero
+ *Return: 0
+ */
+int _abs(int num)
+{
+	if (num >= 0)
+	{
+		return (num);
+	}
+	else
+	{
+		return (-num);
+	}
+}
