6abb8beec7c9e3e1cdf0d4a52f05c4133d5a738f
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-18 19:37:22 +0000

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 /**
- *
+ * print_last_digit - imprime el utimo digito
+ * @n: es el ulimo digito
+ * Return: 0
  */
+int print_last_digit(int n)
+{
+	if (n % 10)
+	{
+		return (n);
+	}
+	else
+	{
+		return (0);
+	}
 
+}
