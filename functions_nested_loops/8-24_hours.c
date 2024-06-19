429effe7c42a6f3202f0121a90a7496aba52def8
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-19 00:47:02 +0000

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
+ * jack_bauer - Imprime la hora
  *
- *
+ * Return: 0
  */
 void jack_bauer(void)
 {
@@ -11,25 +12,33 @@ void jack_bauer(void)
 	int c;
 	int d;
 
-	for (a = 0 ; a <= 2 ; a++)
+		for (a = 0; a <= 2; a++)
 	{
-		_putchar(a);
-	
-	}
-		for (b = 0 ; b <= 3 ; b++)
-		{
-	
-			_putchar(b);
-		}
-	
-	
-				for (c = 0 ; c <= 5 ; c++)
-				{
-					_putchar(c);
-				}
 
-					for (d = 0 ; d <= 9 ; d++)
-					{
-						_putchar(d);
-					}
+
+			for (b = 0; b <= 9 && (a == 2 ? b <= 3 : 1); b++)
+		{
+
+
+
+				for (c = 0; c <= 5; c++)
+			{
+
+
+
+					for (d = 0; d <= 9; d++)
+				{
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
+
+
+				}
+			}
+		}
+	}
+
 }
