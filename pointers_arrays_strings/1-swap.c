10fed2f34c65b84ff65fa4c6944ebdd545258a88
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-19 17:26:11 +0000

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - Intercambia los valores de dos enteros
+ * @a: Un entero
+ * @b: Otro entero
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
