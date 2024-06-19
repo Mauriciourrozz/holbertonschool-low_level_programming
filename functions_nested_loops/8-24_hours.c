8797b370fbeb90c512e575d8e1ce3e85e47c6124
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-19 00:08:17 +0000

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0 ; a <= 2 ; a++)
+	{
+		_putchar(a);
+	
+	}
+		for (b = 0 ; b <= 3 ; b++)
+		{
+	
+			_putchar(b);
+		}
+	
+	
+				for (c = 0 ; c <= 5 ; c++)
+				{
+					_putchar(c);
+				}
+
+					for (d = 0 ; d <= 9 ; d++)
+					{
+						_putchar(d);
+					}
+}
