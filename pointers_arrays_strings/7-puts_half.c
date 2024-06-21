324821c0f7316a04fc603b1f60fc1beb71d09733
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-21 15:04:13 +0000

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,4 +2,28 @@
 /**
  *
  */
+void puts_half(char *str)
+{
+	int n;
+	int a;
 
+	for (n = 0; str[n] != '\0'; n++)
+	{
+
+	}
+	if (n % 2 == 0)
+		a = n / 2;
+
+	else 
+	{
+		a = (n - 1) / 2;
+		a++;
+	}
+	
+	for (; a < n; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');	
+	
+}
