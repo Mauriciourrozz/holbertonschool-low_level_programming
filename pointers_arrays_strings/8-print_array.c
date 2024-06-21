a847725f1690444480469b5b60d48e666ac35374
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-21 18:27:10 +0000

    8-print_array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,8 +1,17 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- *
+ * print_array - Esccribe elementos de una mtriz
+ * @a: a
+ * @n: n
  */
 void print_array(int *a, int n)
 {
-	
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+	}
+	printf("\n");
+}
