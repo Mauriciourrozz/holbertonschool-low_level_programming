5b09ff4f7aa6b30d91800ddcbe86660a10cb0574
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-21 13:28:00 +0000

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,8 +7,17 @@
 void rev_string(char *s)
 {
 	int largo;
-	char aux;
-	int b; 
-	
-	
+	int aux;
+	char cambio; 
 
+	for (largo = 0; s[largo] != '\0'; largo++)
+	{
+	}
+
+	for (aux = 0; aux < largo / 2 - 1; aux++)
+	{
+		cambio = s[aux];
+		s[aux] = s[largo - aux - 1];
+		s[largo - aux - 1] = cambio;
+	}
+}
