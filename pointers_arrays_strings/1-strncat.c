1b084b37f6be99883e6c810ee4f6d910e69f8385
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-24 19:26:52 +0000

    1-strcat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -15,13 +15,10 @@ char *_strncat(char *dest, char *src, int n)
 	for (a = 0; dest[a] != '\0'; a++)
 	{
 	}
-	for (b = 0; src[b] != '\0' && b < n - 1; b++)
+	for (b = 0; src[b] != '\0' && b < n; b++)
 	{
 		dest[a] = src[b];
 		a++;
 	}
-
-	dest[a + b] = '\0';
-
 	return (dest);
 }
