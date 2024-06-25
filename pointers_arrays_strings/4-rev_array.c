a7826d01da5eb0681a11478dc965b2a0e36343dd
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-25 18:27:48 +0000

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- *
+ * reverse_array - invierte un array
+ * @a: Array
+ * @n: Numero de elementos de la matriz
  *
  */
 void reverse_array(int *a, int n)
 {
 	int cambio;
 	int aux = 0;
-	
+
 	for (; aux < n / 2; aux++)
 	{
 		cambio = a[aux];
