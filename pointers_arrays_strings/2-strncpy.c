c28b860729cbc8f7b8103fadc845e8405779b27f
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-25 13:30:26 +0000

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
 /**
- *
- *
+ * _strncpy - copia la cadena
+ * @dest: dest
+ * @src: src
+ * @n: n
+ * Return: 0
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int count;
-	int copy;
 
-	for (count = 0; dest[count] != 0; count++)
+	for (count = 0; count < n && src[count] != '\0'; count++)
 	{
+		dest[count] = src[count];
 	}
-	for (copy = 0; src[copy] != 0 && copy >= n; copy++)
+	for (; count < n; count++)
 	{
-		dest[count] = src[copy];
+		dest[count] = '\0';
 	}
-	return(0);
+
+	return (dest);
 }
