d985716105587351ea1c742a19b5940a8c6401f3
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-25 17:24:42 +0000

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,15 +6,16 @@
  * @s2: otra cadena
  * Return: 0
  */
-int _strcmp(char *s1, char *s2) {
-    int i = 0;
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
 
-    while (s1[i] == s2[i]) {
-        if (s1[i] == '\0')
-            return 0;
-        i++;
-    }
-
-    return s1[i] - s2[i];
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0')
+		return (0);
+		i++;
+	}
+	return (s1[i] - s2[i]);
 }
 
