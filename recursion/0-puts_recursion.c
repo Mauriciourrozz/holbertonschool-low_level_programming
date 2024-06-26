25b248831a75f5f3a7e93829c05dd5d730308e9d
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-26 18:37:05 +0000

    1-print_rev_recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,12 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s == '\0') /* si el caracter esigual a nulo es pq llegamos al final de la cadena */
 	{
-		_putchar('\n');
+		_putchar('\n'); /*imprimimos el salto de linea y retornamos para terminar la recursion*/
 		return;
 	}
 
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	_putchar(*s); /*imprimimos el cara ter actual de la cadena*/
+	_puts_recursion(s + 1); /*llamamos a la funcion recursivamente para que se imprima el resto de la cadena*/
 }
