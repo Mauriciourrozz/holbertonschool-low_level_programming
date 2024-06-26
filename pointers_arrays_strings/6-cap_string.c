6b4bbd5166452dddea349e6e75bc6d28837a0d72
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-26 02:50:11 +0000

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,29 @@
 #include "main.h"
 
 /**
- *
- */
-char *cap_string(char *a)
+*cap_string - Capitalizar cada palabra del string
+*Return: retorna el string
+*@x: string ingresado
+*/
+
+char *cap_string(char *x)
 {
-	
+	int i = 0;
+
+	/*Recorro el string*/
+	while (x[i])
+	{
+		/*Si la primer letra del string es minuscula*/
+		if (x[0] >= 'a' && x[0] <= 'z')
+			x[0] -= 32;
+
+		/* Si hay un espacio en blanco y la siguiente letra es minuscula*/
+		if (x[i] == ' ' || x[i] == '\n' || x[i] == '\t' || x[i] == '.')
+			if (x[i + 1] >= 'a' && x[i + 1] <= 'z')
+				x[i + 1] -= 32;
+
+		i++;
+	}
+
+	return (x);
 }
