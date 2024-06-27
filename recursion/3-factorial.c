f6d725c88936adc98a9ff0d4fede317cbd141dbb
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-27 14:28:17 +0000

    3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -5,16 +5,16 @@
  */
 int factorial(int n)
 {
-	if (n > 0)
-	{
-		return (n);
-	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		return (-1);
 	}
+	else if (n > 0)
+	{
+		return (n);
+	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
