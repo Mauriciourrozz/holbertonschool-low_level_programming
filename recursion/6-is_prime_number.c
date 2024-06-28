7bfad98aac19a2a17111005864c54013e08ca980
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-28 14:53:06 +0000

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,23 +1,31 @@
 #include "main.h"
 
 /**
+ *
+ */
+int num_prim(int n, int i)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (i * i > n)
+	{
+		return (1);
+	}
+	else if (n % i == 0)
+	{
+		return (0);
+	}
+	return (num_prim(n, i + 1));
+}
+
+/**
  * is_prime_number - calcula si un numero es primo
  * @n: numero
  * Return: 1 si es primo 0 si no
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else if (n * n > n)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}	
-	return (n * is_prime_number(n - 1));
+	return (num_prim(n, 2));
 }
