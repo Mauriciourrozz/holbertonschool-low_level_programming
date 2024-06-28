024eeb56731e462d22e447173e3acac9fd8f64c2
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-28 12:33:39 +0000

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -7,14 +7,17 @@
  */
 int is_prime_number(int n)
 {
-	if (n % 2 == 0 && n % 1 == 0)
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n * n > n)
 	{
 		return (1);
 	}
 	else
 	{
 		return (0);
-	}
-
+	}	
 	return (n * is_prime_number(n - 1));
 }
