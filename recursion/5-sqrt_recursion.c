d13db01df0adc4090ee75d9017ad1d77dc2c45cd
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-28 16:13:08 +0000

    5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * raiz_cuad - Calcula la raiz cuadrada de un numero
+ * @n: numero
+ * @x: x
+ * Return: llama a la funcion
+ */
+int raiz_cuad(int n, int x)
+{
+	if (n < x)
+	{
+		return (-1);
+	}
+	else if (n != x * x)
+	{
+		x++;
+	}
+	else
+	{
+		return (x);
+	}
+
+	return (raiz_cuad(n, x));
+}
+
+/**
+ * _sqrt_recursion - Indica si n tiene raiz cuadrada
+ * @n: n
+ * Return: La raiz cuadrada y - 1 si no tiene
+ */
+int _sqrt_recursion(int n)
+{
+	int x = 1;
+
+	return (raiz_cuad(n, x + 1));
+}
