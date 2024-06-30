2b0015462951d18af5e1b6c91f99392cbd98a5d9
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-30 20:54:45 +0000

    2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
 /**
- *
+ * main - donde comienza la funcion
+ * @argc: contiene la cantidad de argumentos
+ * @argv: contiene los argumentos
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", *argv);
+	int a = 0;
+
+	while (a < argc && argv[0])
+	{
+		printf("%s\n", argv[a]);
+		a++;
+	}
 	return (0);
 }
