207953cddcdb66c7527a8a3fd39dfeb29f820dda
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-30 19:53:57 +0000

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - comienzo de la funcion
+ * @argc: contiene el numero de argumentos
+ * @argv: contiene los argumentos
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
