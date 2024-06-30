22d925e77646c799e424839834c2a77486460269
Mauriciourrozz <9366@holbertonstudents.com>
2024-06-30 20:09:08 +0000

    1-args.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]); /*%s porque es el indicativo de printf de string y argv[0] porque argv es quien tiene los argumentos, y en el 0 es donde esta el nombre*/
 	return (0);
 
 }
--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -6,9 +6,9 @@
  * @argv: contiene los argumentos
  * Return: 0
  */
-int main(int argc, char *argv[])
-{
-	(void)argv;
-	printf("%d\n", argc - 1);
+int main(int argc, char *argv[]) /*defino funcion main y parametros */
+{            /*argc contiene la cantidad de argumentos que se pasanal programa y argv los argumentos */
+	(void)argv; /* void argv porque como en el codigo no usamos argv al compilar no nos saltara error */
+	printf("%d\n", argc - 1); /* imprimimos, %d porque es el indicador de un entero y argc porque es quien contiene la cantidad de argumentos, -1 porque argc tambien cuenta el nombre del programa y no queremos eso*/
 	return (0);
 }
