36166db03aa89a92606ab5de1a39493b611bb3a7
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-02 12:25:41 +0000

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 
 /**
- *
+ * _strdup - retorna un puntero
+ * @str: Cadena
+ * Return: dup.
  */
 char *_strdup(char *str)
 {
@@ -10,37 +12,27 @@ char *_strdup(char *str)
 	int len;
 	int i;
 
-	// me fijo si el string que me pasaron es nulo
 	if (str == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
-	// cuento el largo del string que me pasaron
 	for (len = 0; len[str] != '\0'; len++)
 	{
 	}
-	
-	// reservo el espacio dinamicamente dependiendo del string qe me pasaron
+
 	dup = malloc(len + 1 * sizeof(char));
 
-	// me fijo si el malloc fallo
 	if (dup == NULL)
 	{
 		return (NULL);
 	}
 
-	// recorro el dup (el espacio de memoria donde ira la copia del string) y voy asignando los valores correspondientes
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		dup[i] = str[i];
 	}
-
-	// avanzo i para poder colocar el caracter nulo al final :)
 	i++;
 	dup[i] = '\0';
 
-
-
-	// termine xd
 	return (dup);
 }
