6a292456b08e10adc2ad21e51ade377cf5a3c2a7
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-02 12:33:56 +0000

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,26 +12,26 @@ char *_strdup(char *str)
 	int len;
 	int i;
 
-	if (str == NULL)
+	if (str == NULL) /* veo si el string que me pasaron es null */
 	{
 		return (NULL);
 	}
-	for (len = 0; len[str] != '\0'; len++)
+	for (len = 0; len[str] != '\0'; len++) /* cuento el largo del string que me pasaron, tambien se puede hacer asi: size = _strlen(str) + 1 (+1 para que cuente el '\0')*/
 	{
 	}
 
-	dup = malloc(len + 1 * sizeof(char));
+	dup = malloc(len + 1 * sizeof(char)); /* reservo el espacio en la memoria dinamicamente */
 
-	if (dup == NULL)
+	if (dup == NULL) /* pruebo si malloc fallo (por falta de espacio por ej.) */
 	{
 		return (NULL);
 	}
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++) /* recorro el dupy voy asignando los valores correspondientes */
 	{
 		dup[i] = str[i];
 	}
-	i++;
+	i++;  /* avanzo i para poder colocar el caracter nulo*/
 	dup[i] = '\0';
 
 	return (dup);
