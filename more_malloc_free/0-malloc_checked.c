d65e0a61e41c8f14b236e261e47a15b39aabc437
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-04 13:32:35 +0000

    0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - asigna memoria
+ * @b: b
+ * Return: puntero a la memoria asignada
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *checked;
+
+	checked = malloc(b * sizeof(int *));
+
+	if (checked == NULL)
+	{
+		return (NULL);
+	}
+	return (checked);
+
+}
