93bdc93c4f302f328a072e1f597b7198c3e68120
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-04 14:10:02 +0000

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -9,13 +9,14 @@ void *malloc_checked(unsigned int b)
 {
 	int *checked;
 
-	checked = malloc(b * sizeof(int));
+	checked = malloc(b * sizeof(int *));
 
 	if (checked == NULL)
 	{
 		free(checked);
 		exit(98);
 	}
+	free(checked);
 	return (checked);
 
 }
