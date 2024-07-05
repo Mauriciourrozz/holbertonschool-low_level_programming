b55d67a9a2ad977ca8b071bf67874785d5eb578f
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-05 17:16:22 +0000

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,9 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *
+ * _calloc - Asigna memoria para un puntero
+ * @nmemb: Es la serie que le queremos asignar memoria
+ * @size: Es el tama√o de bytes que queremos devolver de nmemb
+ * Return: el puntero a la memoria asignada
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int a;
+	int *calloc;
 
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	a = nmemb * size;
+
+	calloc = malloc(a);
+
+	if (calloc == NULL)
+	{
+		return (NULL);
+	}
+
+	return (calloc);
 }
