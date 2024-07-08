b8bff3229c8b82bff08980899a5ca959e4c4e565
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-08 17:35:07 +0000

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 	{
-		printf("name: (nil)\n");
+		printf("Name: (nil)\n");
 	}
 	else
 	{
@@ -25,7 +25,7 @@ void print_dog(struct dog *d)
 
 	if (d->owner == NULL)
 	{
-		printf("owner: (nil)\n");
+		printf("Owner: (nil)\n");
 	}
 	else
 	{
