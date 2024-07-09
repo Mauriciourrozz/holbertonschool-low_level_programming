c47aedd86cc93e4ee3cafe8aee37335a102cd056
Mauriciourrozz <you@example.com>
2024-07-09 21:47:06 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,8 +1,57 @@
 #include "dog.h"
+#include <stdlib.h>
+#include <string.h>
 /**
  *
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	
+	int largoname, largoowner, i;
+	dog_t *newdog;
+
+	for (largoname = 0; name[largoname] != '\0'; largoname++)
+	{}
+	for (largoowner = 0; name[largoowner] != '\0'; largoowner++)
+	{}
+
+	newdog = (dog_t *)malloc(sizeof(dog_t));
+	if (newdog == NULL)
+	{
+		return (NULL);
+	}
+
+	newdog->name = NULL;
+	newdog->owner = NULL;
+
+	newdog->name =(char *)malloc((largoname + 1) * sizeof(char));
+	newdog->name =(char *)malloc((largoowner + 1) * sizeof(char));
+
+	if (newdog->name == NULL || newdog->owner == NULL)
+	{
+		if (newdog->name != NULL)
+		{
+			free(newdog->name);
+		}
+		if (newdog->owner != NULL)
+		{
+			free(newdog->owner);
+		}
+		free(newdog);
+		return (NULL);
+	}
+
+	for (i = 0; i < largoname; i++)
+	{
+		newdog->name[i] = name[i];
+	}
+
+	for (i = 0; i < largoowner; i++)
+	{
+		newdog->owner[i] = owner[i];
+	}
+
+	newdog->age = age;
+
+	return (newdog);
+
 }
