899d47b6bb5eab768f787464fb45b334e2553d48
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-11 16:01:54 +0000

    2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+ * int_index - busca un numero entero
+ * @array: array
+ * @size: tama√o
+ * @cmp: puntero a la funcion
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if ( array == NULL || size <= 0)
+	{
+		return (-1);
+	}
+
+	for (a = 0; a < size; a++)
+	{
+		if (cmp(array[a]))
+		{
+			return (a);
+		}
+	}
+	return (0);
+}
