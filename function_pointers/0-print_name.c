6db09a14d3045792842ee9d1146e83180ae98c3a
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-11 13:06:25 +0000

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,9 +1,11 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- *
+ * print_name - imprime el nombre
+ * @name: nombre
+ * @f: es el puntero a la funcion
  */
 void print_name(char *name, void (*f)(char *))
 {
-	
+	f(name);
 }
