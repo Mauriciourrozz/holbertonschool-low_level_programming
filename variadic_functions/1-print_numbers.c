ee67bbb06047dbaa56e051341a23124fd71a0cc6
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-14 22:29:56 +0000

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -8,15 +8,15 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list argum;
-	unsigned int i;
+	va_list argum; // se declara para ver la lista de argumentos
+	unsigned int i; // para el bucle
 
-	va_start(argum, n);
+	va_start(argum, n); //inicializa argum para que apunte a n 
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++) // mientras i , n accede a cada lugar de n y se imprime
 	{
-		printf("%d", va_arg(argum, unsigned int));
-		if (i < n - 1 && separator != NULL)
+		printf("%d", va_arg(argum, unsigned int)); // %d porque es un entero y el resto indica que queremos que sea ese enero, en este caso obtiene el siguiente argumento de tipo unsigned int y lo imprime
+		if (i < n - 1 && separator != NULL) // i < n - 1 es porque la , y el espacio lo imprimira en todos menos el ultimo numero (caso borde)
 		{
 			printf("%s", separator);
 		}
