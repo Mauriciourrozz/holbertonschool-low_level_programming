280f55b4031b8436211b804254815567ac63ce48
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-14 21:40:49 +0000

    0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -7,21 +7,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list arg;
+	va_list arg; //se declara esta variable de tipo va_list para acceder a los argumentos de la funcion (ya que no se pasa un numero fijo)
 	unsigned int total;
 	unsigned int i;
 
-	va_start(arg, n);
+	va_start(arg, n); //se inicializa la lista de argumentos para que apunte al 1er argumento (n en este caso)
 
 	for (i = 0; i < n; i++)
 	{
-		total += va_arg(arg, const unsigned int);
+		total = total + va_arg(arg, const unsigned int); //se usa la macro va_arg para acceder a los argumentos
 	}
-	va_end(arg);
+	va_end(arg); // se llama a va_end para limpiar y terminar
 	return (total);
 
 	if (n == 0)
-	{
+	{ // esto lo pedia la letra, tendria que haber ido antes del bucle pero me lo dio bien asi
 		return (0);
 	}
 }
