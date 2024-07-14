dde57ad8642d88ab737457621be56795e4bbbb5a
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-14 22:15:45 +0000

    1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - imprime numeros
+ * @separator: es lo que se imprime entre los num, (, )
+ * @n: son los numeros
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list argum;
+	unsigned int i;
+	unsigned int total;
+
+	va_start(argum, n);
+
+	if (separator == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		total = total + va_arg(argum, unsigned int);
+		printf("%d", total);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(argum);
+	printf("\n");
+}
