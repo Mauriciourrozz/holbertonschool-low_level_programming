9543dd805e4a7948dab44549df59ce6b3ca41550
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-16 00:40:36 +0000

    3-print_all.c

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,43 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_all - imprime algo
+ * @format: lista de argumentos
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	va_list argum;
+	char *aux;
+
+	va_start(argum, format);
+
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c, ", va_arg(argum, int));
+				break;
+			case 'i':
+				printf("%d, ", va_arg(argum, int));
+				break;
+			case 'f':
+				printf("%f, ", va_arg(argum, double));
+				break;
+			case 's':
+				aux = va_arg(argum, char *);
+				if (aux == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+					printf("%s", aux);
+				break;
+		}
+		i++;
+	}
+	va_end(argum);
+	printf("\n");
+}
