6af319dd7425a4ae60b5e31247d8c1af7922a450
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-24 13:38:26 +0000

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - imprimir las tablas del 1 al 9
+ * Return: Nada
+ */
+void times_table(void)
+{
+	int fila = 10;
+	int i;
+	int j;
+	int resultado;
+
+	for (i = 0; i < fila; i++)
+	{
+		for (j = 0; j < fila; j++)
+		{
+			resultado = i * j;
+
+			if (resultado < 10)
+			{
+				_putchar(' ');
+				_putchar(resultado + '0');
+			}
+			else
+			{
+				_putchar(resultado / 10 + '0');
+				_putchar(resultado % 10 + '0');
+			}
+			if (j != fila - 1)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
