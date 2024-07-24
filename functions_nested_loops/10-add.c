c0509df870658af933e40a5f432c56850dcde357
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-24 14:14:45 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Suma dos numeros enteros
+ * @a: un numero
+ * @b: el otro numero
+ * Return: La suma de los dos enteros
+ */
+int add(int a, int b)
+{
+	int suma;
+
+	suma = a + b;
+
+	return (suma);
+}
