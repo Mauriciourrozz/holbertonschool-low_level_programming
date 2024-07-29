b4d56421eb7e840dc3a4c1e1f82343accf310e5a
Mauriciourroz <9366@holbertonstudents.com>
2024-07-29 19:02:29 +0000

    4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - libera la lista list_t
+ * @head: Head
+ * Return: nada
+ */
+void free_list(list_t *head)
+{
+	list_t *a;
+
+	while (head != NULL)
+	{
+		a = head->next;
+		free(head);
+		head = a;
+	}
+}
