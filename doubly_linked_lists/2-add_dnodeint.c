82eeab64efdf83d21f6ba61a366b268e196b4a7f
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-30 15:16:25 +0000

    2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,22 +8,22 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *nuevonodo = malloc(sizeof(dlistint_t));
+	dlistint_t *nuevonodo = malloc(sizeof(dlistint_t)); //reservamos memoria para un nuevo nodo
 
-	if (nuevonodo == NULL)
+	if (nuevonodo == NULL) //si la asignacion fala retorna NULL
 	{
 		return (NULL);
 	}
 
-	nuevonodo->n = n;
-	nuevonodo->prev = NULL;
-	nuevonodo->next = *head;
+	nuevonodo->n = n; //asignamos el valor al nuevo nodo (n ya esta definido y le asignamos el valor)
+	nuevonodo->prev = NULL; //asignamos primer lugar al nodo, no tiene nodo anterior
+	nuevonodo->next = *head; //el siguiente nodo sera el primer nodo de la lista(osea elnodo que estaba antes en lacabeza)
 
-	if (*head != NULL)
+	if (*head != NULL) //si la lista no estaba vacia, se actualiza 'prev' del antiguo 1er nodo para que apunte al nuevo
 	{
 		(*head)->prev = nuevonodo;
 	}
-	*head = nuevonodo;
+	*head = nuevonodo; //Se actualiza el puntero a la cabeza de la lista para que apunte al nuevo nodo, que ahora es el primer nodo de la lista.
 
 	return (nuevonodo);
 }
