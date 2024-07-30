1f1af6ccfe2e6475ecaeee8567be817536ff7160
Mauriciourrozz <9366@holbertonstudents.com>
2024-07-30 19:21:09 +0000

    4-free_dlistint.c

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -7,9 +7,9 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	if (head == NULL)
+	if (head == NULL) //me aseguro de que head no sea null (que no este vacio)
 		return;
 
-	free_dlistint(head->next);
-	free(head);
+	free_dlistint(head->next); //llamo a la funcion recursivamente mientras avanzo en los nodos siempre que sea distinto de NULL
+	free(head); //Libero el espacio
 }
